a444d152ce9b0788c13611444625f1914b6978cf
Polina Nikitenko <nikitpolina@yandex.ru>
2022-07-09 16:21:06 +0300

    add tests for class Bun

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,30 @@
+package praktikum;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BunTest {
+    private Bun bun;
+
+    @Before
+    public void initObject() {
+        bun = new Bun("black bun", 100);
+    }
+
+    @Test
+    public void getNameTest() {
+        String actual = bun.getName();
+        String expected = "black bun";
+        assertEquals("Failed to get bun name", expected, actual);
+    }
+
+    @Test
+    public void getPriceTest() {
+        Float actual = bun.getPrice();
+        Float expected = 100f;
+        assertEquals("Failed to get bun price", expected, actual);
+    }
+
+
+}
